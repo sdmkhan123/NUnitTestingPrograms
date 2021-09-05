89c16d7ea00927ced0add7593a1199e097d904f8
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-05 23:59:03 +0530

    Find the square root of a num using Newton's method

--- Program.cs
+++ Program.cs
@@ -4,28 +4,25 @@ namespace NUnitTestingPrograms
 {
     class Program
     {
-        static double monthlyPayment(double P, double R, double Y)
+        static double squareRoot(int c)
         {
-            double n = 12 * Y;
-            double r = R / (12 * 100);
-            double payment = P * r / (1 - (1 / (Math.Pow(1 + r, n))));
-            return payment;
+            double epsilon = 1e-15;
+            double t = c;
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            return t;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to NUnit Testing Programs");
 
-            Console.WriteLine("Enter Principal amount ");
-            double P = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Non-Negative number ");
+            int c = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Rate Percent per year ");
-            double R = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter number of years ");
-            double Y = Convert.ToInt32(Console.ReadLine());
-
-            double monthlyGetPayment = monthlyPayment(P, R, Y);
-            Console.WriteLine("The monthly Payment is " + monthlyGetPayment);
+            double toGetSquareRoot = squareRoot(c);
+            Console.WriteLine("Square root of a non-negative number using Newton's method is: " + toGetSquareRoot);
         }
     }
 }
