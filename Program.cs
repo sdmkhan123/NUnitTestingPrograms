9ec2a22640ee656c3ac3a3970deab453e0e6ae8e
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-05 23:15:32 +0530

    Convert the input fahrenheit temp into Celsius or vice versa

--- Program.cs
+++ Program.cs
@@ -4,26 +4,30 @@ namespace NUnitTestingPrograms
 {
     class Program
     {
-       public static int dayOfWeek(int date, int month, int year)
+        public static double celsiusToFahrenheit(double celsiusTemp)
         {
-            int y0 = year - ( 14 - month ) / 12;
-            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
-            int m0 = month + 12 * ((14 - month) / 12) - 2;
-            int d0 = (date + x + (31 * m0) / 12) % 7;
-            return d0;
+            double inFahrenheit = (celsiusTemp * 9 / 5) + 32;
+            return inFahrenheit;
+        }
+        public static double fahrenheitToCelsius(double fahrenheitTemp)
+        {
+            double inCelsius = (fahrenheitTemp - 32) * 5 / 9;
+            return inCelsius;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to NUnit Testing Programs");
-            Console.WriteLine("Enter Date ");
-            int date = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Month ");
-            int month = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Year ");
-            int year = Convert.ToInt32(Console.ReadLine());
-            string[] daysName = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-            int day = dayOfWeek(month, date, year);
-            Console.WriteLine(daysName[day]);
+
+            Console.WriteLine("Enter temp in Celsius ");
+            double celsiusTemp = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter temp in Fahrenheit ");
+            double fahrenheitTemp = Convert.ToInt32(Console.ReadLine());
+            
+            double fahrenheit = celsiusToFahrenheit(celsiusTemp);
+            double celsius = fahrenheitToCelsius(fahrenheitTemp);
+            Console.WriteLine("convert the temp from celsius to fahrenheit is " + fahrenheit + "°F");
+            Console.WriteLine("convert the temp from fahrenheit to celsius is " + celsius + "°C");
         }
     }
 }
