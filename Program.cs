f6e621abba6380e75dd018084449ff6a44b03a30
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-05 23:41:20 +0530

    User get Monthly Payment

--- Program.cs
+++ Program.cs
@@ -4,30 +4,28 @@ namespace NUnitTestingPrograms
 {
     class Program
     {
-        public static double celsiusToFahrenheit(double celsiusTemp)
+        static double monthlyPayment(double P, double R, double Y)
         {
-            double inFahrenheit = (celsiusTemp * 9 / 5) + 32;
-            return inFahrenheit;
-        }
-        public static double fahrenheitToCelsius(double fahrenheitTemp)
-        {
-            double inCelsius = (fahrenheitTemp - 32) * 5 / 9;
-            return inCelsius;
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+            double payment = P * r / (1 - (1 / (Math.Pow(1 + r, n))));
+            return payment;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to NUnit Testing Programs");
 
-            Console.WriteLine("Enter temp in Celsius ");
-            double celsiusTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Principal amount ");
+            double P = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter temp in Fahrenheit ");
-            double fahrenheitTemp = Convert.ToInt32(Console.ReadLine());
-            
-            double fahrenheit = celsiusToFahrenheit(celsiusTemp);
-            double celsius = fahrenheitToCelsius(fahrenheitTemp);
-            Console.WriteLine("convert the temp from celsius to fahrenheit is " + fahrenheit + "°F");
-            Console.WriteLine("convert the temp from fahrenheit to celsius is " + celsius + "°C");
+            Console.WriteLine("Enter Rate Percent per year ");
+            double R = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number of years ");
+            double Y = Convert.ToInt32(Console.ReadLine());
+
+            double monthlyGetPayment = monthlyPayment(P, R, Y);
+            Console.WriteLine("The monthly Payment is " + monthlyGetPayment);
         }
     }
 }
