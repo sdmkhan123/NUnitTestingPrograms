61efdfced3f2fa53197ce12e41cafb485a34df99
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-06 00:38:25 +0530

    to change Dicimal into Binary number

--- Program.cs
+++ Program.cs
@@ -4,25 +4,41 @@ namespace NUnitTestingPrograms
 {
     class Program
     {
-        static double squareRoot(int c)
+        static int toBinary(int decimalNnumber)
         {
-            double epsilon = 1e-15;
-            double t = c;
-            while (Math.Abs(t - c / t) > epsilon * t)
+            if (decimalNnumber == 0)
             {
-                t = (c / t + t) / 2.0;
+                return 0;
             }
-            return t;
+            else
+            {
+                return (decimalNnumber % 2 + 10 * toBinary(decimalNnumber / 2));
+            }   
         }
+
+        /* Iterative method using array 
+     
+        static void toBinary(int decimalNnumber)
+        {
+            int[] binaryNum = new int[32];
+            int i = 0;
+            while (decimalNnumber > 0)
+            {
+                binaryNum[i] = decimalNnumber % 2;
+                decimalNnumber = decimalNnumber / 2;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+                Console.Write(binaryNum[j]);
+        }
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to NUnit Testing Programs");
 
-            Console.WriteLine("Enter a Non-Negative number ");
-            int c = Convert.ToInt32(Console.ReadLine());
-
-            double toGetSquareRoot = squareRoot(c);
-            Console.WriteLine("Square root of a non-negative number using Newton's method is: " + toGetSquareRoot);
+            Console.WriteLine("Enter a decimal number ");
+            int decimalNnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(toBinary(decimalNnumber));
         }
     }
 }
